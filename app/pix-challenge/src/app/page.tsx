748d8730d4429de03036d71e4b3d84988d716d21
Marcos Trindade <marcosguimaraest04@gmail.com>
2024-11-03 15:05:05 -0300

    move cobrança dialog to compontents, delete /cobranca endpoint

--- app/pix-challenge/src/app/page.tsx
+++ app/pix-challenge/src/app/page.tsx
@@ -1,9 +1,22 @@
+import Cobranca from "@/components/ui/dialog_cobranca";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      HOME
+      <div className="container mx-auto h-full">
+                <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 xl:pl-20 xl:pr-20">
+                    <div className="text-center xl:text-left">
+                        <h1 className="text-2xl font-bold">
+                            _SUAS TRANSAÇÕES
+                        </h1>
+                    </div>
+                    <div className="flex flex-row flex-wrap xl:flex-row items-center justify-center">
+                        <Cobranca>
+                        </Cobranca>
+                    </div>
+                </div>
+            </div>
     </main>
   );
 }
